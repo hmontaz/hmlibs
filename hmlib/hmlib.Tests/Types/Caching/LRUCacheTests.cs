5741f5105c53471d4c685bdef04e8bc3a5ae76d4
Hossein Montazeri <h.montaz.ca@gmail.com>
2025-07-31 12:12:10 -0700

    LRUCache

---
+++ hmlib/hmlib.Tests/Types/Caching/LRUCacheTests.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+using hmlib.Types.Caching;
+
+namespace hmlib.Tests.Types.Caching
+{
+	public class LRUCacheTests
+	{
+		[Fact]
+		public void LRUCache_AddAndRetrieveItems()
+		{
+			var cache = new LRUCache<int, string>(3);
+			cache.Put(1, "One");
+			cache.Put(2, "Two");
+			cache.Put(3, "Three");
+			cache.Get(1).Should().Be("One");
+			cache.Get(2).Should().Be("Two");
+			cache.Get(3).Should().Be("Three");
+		}
+		[Fact]
+		public void LRUCache_EvictionTest()
+		{
+			var cache = new LRUCache<int, string>(2);
+			cache.Put(1, "One");
+			cache.Put(2, "Two");
+			cache.Get(1).Should().Be("One");
+			cache.Get(2).Should().Be("Two");
+			// Adding a new item should evict the least recently used item (1)
+			cache.Put(3, "Three");
+			cache.Get(1).Should().Be(null);
+			cache.Get(2).Should().Be("Two");
+			cache.Get(3).Should().Be("Three");
+		}
+		[Fact]
+		public void LRUCache_UpdateExistingItem()
+		{
+			var cache = new LRUCache<int, string>(2);
+			cache.Put(1, "One");
+			cache.Put(2, "Two");
+			cache.Get(1).Should().Be("One");
+			cache.Get(2).Should().Be("Two");
+			// Update item 1
+			cache.Put(1, "Updated One");
+			cache.Get(1).Should().Be("Updated One");
+			cache.Get(2).Should().Be("Two");
+		}
+		[Fact]
+		public void LRUCache_EmptyCache()
+		{
+			var cache = new LRUCache<int, string>(2);
+			cache.Get(1).Should().Be(null); // Should return null for non-existent key
+			cache.Put(1, "One");
+			cache.Get(1).Should().Be("One"); // Should return "One" after adding it
+			cache.Put(2, "Two");
+			cache.Put(3, "Three"); // This should evict "One"
+			cache.Get(1).Should().Be(null); // Should return null for evicted key
+			cache.Get(2).Should().Be("Two");
+			cache.Get(3).Should().Be("Three");
+		}
+		[Fact]
+		public void LRUCache_ClearCache()
+		{
+			var cache = new LRUCache<int, string>(3);
+			cache.Put(1, "One");
+			cache.Put(2, "Two");
+			cache.Put(3, "Three");
+			cache.Clear();
+			cache.Get(1).Should().Be(null); // Should return null after clearing
+			cache.Get(2).Should().Be(null);
+			cache.Get(3).Should().Be(null);
+		}
+	}
+}
---
+++ hmlib/hmlib/Types/Caching/LRUCache.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hmlib.Types.Caching
+{
+	/// <summary>
+	/// 
+	/// </summary>
+	/// <typeparam name="TKey"></typeparam>
+	/// <typeparam name="TValue"></typeparam>
+	public class LRUCache<TKey, TValue>
+	{
+		Dictionary<TKey, LinkedListNode<(TKey key, TValue value)>> _cache;
+		LinkedList<(TKey key, TValue value)> _list;
+		int _capacity;
+
+		/// <summary>
+		/// Initializes a new instance of the LRUCache with a specified capacity.
+		/// </summary>
+		/// <param name="capacity">The maximum number of items the cache can hold.</param>
+		public LRUCache(int capacity)
+		{
+			_capacity = capacity;
+			_cache = new Dictionary<TKey, LinkedListNode<(TKey, TValue)>>();
+			_list = new LinkedList<(TKey, TValue)>();
+		}
+
+		/// <summary>
+		/// Retrieves an item from the cache by its key.
+		/// </summary>
+		/// <param name="key"></param>
+		/// <returns>The value associated with the key, or default(TValue) if not found.</returns>
+		public TValue Get(TKey key)
+		{
+			if (!_cache.ContainsKey(key)) return default(TValue);
+
+			var node = _cache[key];
+			_list.Remove(node);
+			_list.AddLast(node);
+
+			return node.Value.value;
+		}
+
+		/// <summary>
+		/// Adds or updates an item in the cache.
+		/// </summary>
+		/// <param name="key">The key of the item.</param>
+		/// <param name="value">The value of the item.</param>
+		public void Put(TKey key, TValue value)
+		{
+			if (_cache.ContainsKey(key))
+			{
+				var node = _cache[key];
+				_list.Remove(node);
+			}
+			else
+			{
+				if (_capacity == _list.Count)
+				{
+					var n = _list.First;
+					_list.Remove(n);
+					_cache.Remove(n.Value.key);
+				}
+			}
+			var newNode = _list.AddLast((key, value));
+			_cache[key] = newNode;
+		}
+
+		/// <summary>
+		/// Clears the cache, removing all items.
+		/// </summary>
+		public void Clear()
+		{
+			_cache.Clear();
+			_list.Clear();
+		}
+	}
+}
